07bdbc0d227f6036c770294075534c3c0003b368
Michal Stehlík <michal.stehlik@pslib.cz>
2021-10-20 12:09:55 +0200

    searching

---
+++ Classrooms2021/Areas/Students/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Classrooms2021.Data;
+using Classrooms2021.Models;
+
+namespace Classrooms2021.Areas.Students.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Classrooms2021.Data.AppDbContext _context;
+
+        public IndexModel(Classrooms2021.Data.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Student> Student { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string Search { get; set; }
+
+        public void OnGet(string classname)
+        {
+            IQueryable<Student> students = _context.Students;
+            if (!String.IsNullOrEmpty(Search))
+            {
+                students = students.Where(s => (s.Firstname.Contains(Search) || (s.Lastname.Contains(Search))));
+            }
+            Student = students
+                .Include(s => s.Classroom)
+                .ToList();
+        }
+    }
+}
